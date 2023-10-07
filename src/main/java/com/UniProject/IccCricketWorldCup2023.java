0b92cfbacedb72962d72902c67aed728709bc6e6
Adib <adib3552@gmail.com>
2023-10-07 16:21:16 +0600

    First Commit

---
+++ src/main/java/com/UniProject/IccCricketWorldCup2023.java
@@ -0,0 +1,13 @@
+package com.UniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IccCricketWorldCup2023 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IccCricketWorldCup2023.class, args);
+	}
+
+}
